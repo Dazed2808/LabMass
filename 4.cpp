e346ce02b3325815063de840864be4ecfaf5a4bf
Denys <111125946+Dazed2808@users.noreply.github.com>
2023-12-28 14:21:19 +0200

    4.cpp

--- 4.cpp
+++ 4.cpp
@@ -5,30 +5,24 @@ void duplicateZeros(std::vector<int>& arr) {
     int original_size = arr.size();
     int zeros_to_duplicate = 0;
 
-    // Лічильник кількості нулів, які потрібно дублювати
     for (int num : arr) {
         if (num == 0) {
             zeros_to_duplicate++;
         }
     }
 
-    // Ітерація з кінця масиву
     for (int i = original_size - 1; i >= 0; --i) {
         if (arr[i] == 0) {
-            // Дублювати нулі
             if (i + zeros_to_duplicate < original_size) {
                 arr[i + zeros_to_duplicate] = 0;
             }
 
             zeros_to_duplicate--;
 
-            // Якщо після дублювання нулів ми виходимо за межі масиву,
-            // то забезпечимо, що нуль видалиться
             if (i + zeros_to_duplicate < original_size) {
                 arr[i + zeros_to_duplicate] = 0;
             }
         } else {
-            // Простий переміщення чисел вправо
             if (i + zeros_to_duplicate < original_size) {
                 arr[i + zeros_to_duplicate] = arr[i];
             }
@@ -37,7 +31,6 @@ void duplicateZeros(std::vector<int>& arr) {
 }
 
 int main() {
-    // Приклади використання:
     std::vector<int> arr1 = {1, 0, 2, 3, 0, 4, 5, 0};
     duplicateZeros(arr1);
     std::cout << "Приклад 1:";
